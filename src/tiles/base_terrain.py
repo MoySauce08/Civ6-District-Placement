47c1b12e87ab67b7e791247acc4d7c17f90a6203
Daniel Moy <daniel.moy@corusent.com>
2020-10-13 23:31:09 -0400

    Added tile types.

---
+++ src/tiles/base_terrain.py
@@ -0,0 +1,49 @@
+grassland = {
+    'food': 2
+}
+
+grassland_hills = {
+    'food': 2,
+    'production': 1
+}
+
+plains = {
+    'food': 1,
+    'production': 1
+}
+
+plains_hills = {
+    'food': 1,
+    'production': 2
+}
+
+desert = {}
+
+desert_hills = {
+    'production': 1
+}
+
+tundra = {
+    'food': 1
+}
+
+tundra_hills = {
+    'food': 1,
+    'production': 1
+}
+
+snow = {}
+
+snow_hills = {
+    'production': 1
+}
+
+coast = {
+    'food': 1,
+    'gold': 1
+}
+
+lake = {
+    'food': 1,
+    'gold': 1
+}
---
+++ src/tiles/terrain_features.py
@@ -0,0 +1,41 @@
+woods = {
+    'production': 1
+}
+
+rainforest = {
+    'food': 1
+}
+
+marsh = {
+    'food': 1
+}
+
+river = {}
+
+floodplains = {
+    'food': 3
+}
+
+oasis = {
+    'food': 3,
+    'gold': 1
+}
+
+cliffs = {}
+
+reef = {
+    'food': 1,
+    'production': 1
+}
+
+ice = {}
+
+cataract = {}
+
+volcano = {}
+
+volcano_soil = {}
+
+geothermal_fissure = {
+    'science': 1
+}
